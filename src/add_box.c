3068fa20e6b24e62bf9d7d0fcb9d0ac023ddc915
rohith <rohith.pandith@ltts.com>
2021-02-12 17:29:48 +0530

    modifying add_box.c

--- src/add_box.c
+++ src/add_box.c
@@ -1,7 +1,23 @@
 
 #include "box_list.h"
 
-int add_box(box_t *head,int id,float length,float breadth,float){
+int add_a_box(box_t* head,box_data_t data){
 
-	if (*head==NULL){
-		*
+	struct box *newbox= malloc(sizeof(struct box));
+	newbox->data =	data;
+	newbox->next_box =NULL;
+	
+	if(*head==NULL)
+		*head = newbox;
+
+	else{
+		struct box *lastnode =	*head;
+		
+		while(lastnode->next_box ! =NULL){
+			lastnode =lastnode->next_box;
+		}
+		lastnode->next_box=newbox;
+	}
+	
+}	
+
