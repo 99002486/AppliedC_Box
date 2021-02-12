93759fe7abb39de3a9de32222e7504148d2eab0a
minand-n-m <minandnellipunath.manomohanan@ltts.com>
2021-02-12 16:22:09 +0530

    Add box_list header

---
+++ inc/box_list.h
@@ -0,0 +1,18 @@
+#ifndef __BOX_LIST_H__
+#define __BOX_LIST_H__
+
+#include "init_box.h"
+/**
+*@params-Pointer to list
+*@function-adds a box to end of list
+*@return-signifying successfull execution of function
+*/
+int add_a_box(box_t* head);
+
+int display_all(box_t* head);
+
+int box_given_id(box_t* head,COLORS color);
+
+int remove_box_id(box_t* head, int id);
+
+#endif
