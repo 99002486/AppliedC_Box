7ff143fe4e245a838d407cdec54c10bf170d97f7
rohith <rohith.pandith@ltts.com>
2021-02-16 15:04:37 +0530

    add flag bit to box_id.c

--- src/box_id.c
+++ src/box_id.c
@@ -2,15 +2,16 @@
 
 int find_box_id(box_t* head,int id){
     box_t* current = head;
-	
+    int flag=0;	
     if(current == NULL){
 	printf("There are no nodes currently, please add the nodes then search for it\n");
-	return 0;
+	return -1;
     }
 
     while(current != NULL)
     {
         if((current->data.box_id)==id){
+		flag=1;
 		printf("Box id is %d |",current->data.box_id);
 		printf(" Box length is %.2f |",current->data.length);
 		printf(" Box breadth is %.2f|",current->data.breadth);
@@ -19,6 +20,12 @@ int find_box_id(box_t* head,int id){
 		printf(" Box colour is %s\n",current->data.color);            
         }
         current=current->next_box;
+
+    }
+    
+    if(flag==0){
+	printf("Given id= %d did not match with any nodes",id);
+	return 0;
     }
 		
   
