1369ef4610149d4e040862e31723bd6da32343a7
rohith <rohith.pandith@ltts.com>
2021-02-16 11:53:49 +0530

    adding proper return to display_boxes.c

--- src/display_boxes.c
+++ src/display_boxes.c
@@ -6,17 +6,22 @@ int display_all(box_t* head){
 
 	printf("Printing each state of the boxes\n");
 
+	if(disp ==NULL)
+		printf("NULL\n");
 	while(disp != NULL){
-		printf("Box id is %d\n",disp->data.box_id);
-		printf("Box length is %f\n",disp->data.length);
-		printf("Box breadth is %f\n",disp->data.breadth);
-		printf("Box height is %f\n",disp->data.height);
-		printf("Box weight is %f\n",disp->data.weight);
-		printf("Box colour is %d\n",disp->data.color);
+		printf("Box id is %d |",disp->data.box_id);
+		printf(" Box length is %.2f |",disp->data.length);
+		printf(" Box breadth is %.2f|",disp->data.breadth);
+		printf(" Box height is %.2f |",disp->data.height);
+		printf(" Box weight is %.2f |",disp->data.weight);
+		printf(" Box colour is %s\n",disp->data.color);
 
 		disp=disp->next_box;
 	}
-	printf("NULL\n");
+	
+	return 0;	
+
+	
 
 }
 
