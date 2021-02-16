d02013e78112225e44a15f8e25c88efb0aab304a
rohith <rohith.pandith@ltts.com>
2021-02-16 10:58:58 +0530

    modifying src files

--- src/display_boxes.c
+++ src/display_boxes.c
@@ -7,16 +7,16 @@ int display_all(box_t* head){
 	printf("Printing each state of the boxes\n");
 
 	while(disp != NULL){
-		printf("Box id is %d\n",disp->data->box_id);
-		printf("Box length is %f\n",disp->data->length);
-		printf("Box breadth is %f\n",disp->data->breadth);
-		printf("Box height is %f\n",disp->data->height);
-		printf("Box weight is %f\n",disp->data->weight);
-		printf("Box colour is %d\n",disp->data->color);
+		printf("Box id is %d\n",disp->data.box_id);
+		printf("Box length is %f\n",disp->data.length);
+		printf("Box breadth is %f\n",disp->data.breadth);
+		printf("Box height is %f\n",disp->data.height);
+		printf("Box weight is %f\n",disp->data.weight);
+		printf("Box colour is %d\n",disp->data.color);
 
 		disp=disp->next_box;
 	}
-	printf"NULL\n");
+	printf("NULL\n");
 
 }
 
